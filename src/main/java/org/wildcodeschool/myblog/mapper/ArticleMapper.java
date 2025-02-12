c016e3b069c07d38435ff90f1d5b5f8a2e4b5eb3
MatCabdel <chauveau.math@gmail.com>
2025-02-12 12:12:40 +0100

    quête 12 ok

--- src/main/java/org/wildcodeschool/myblog/mapper/ArticleMapper.java
+++ src/main/java/org/wildcodeschool/myblog/mapper/ArticleMapper.java
@@ -1,10 +1,10 @@
 package org.wildcodeschool.myblog.mapper;
 
 import org.springframework.stereotype.Component;
+import org.wildcodeschool.myblog.dto.ArticleCreateDTO;
 import org.wildcodeschool.myblog.dto.ArticleDTO;
 import org.wildcodeschool.myblog.dto.AuthorDTO;
-import org.wildcodeschool.myblog.model.Article;
-import org.wildcodeschool.myblog.model.Image;
+import org.wildcodeschool.myblog.model.*;
 
 import java.util.stream.Collectors;
 
@@ -37,5 +37,41 @@ public class ArticleMapper {
                     .collect(Collectors.toList()));
         }
         return articleDTO;
+    }
+
+    public Article convertToEntity(ArticleCreateDTO articleCreateDTO) {
+        Article article = new Article();
+        article.setTitle(articleCreateDTO.getTitle());
+        article.setContent(articleCreateDTO.getContent());
+
+        // Associer la catégorie
+        Category category = new Category();
+        category.setId(articleCreateDTO.getCategoryId());
+        article.setCategory(category);
+
+        // Convertir la liste d'images
+        article.setImages(articleCreateDTO.getImages().stream()
+                .map(imageDTO -> {
+                    Image image = new Image();
+                    image.setId(imageDTO.getId());
+                    image.setUrl(imageDTO.getUrl());
+                    return image;
+                })
+                .collect(Collectors.toList()));
+
+        // Convertir la liste des auteurs avec leurs contributions
+        article.setArticleAuthors(articleCreateDTO.getAuthors().stream()
+                .map(authorContributionDTO -> {
+                    ArticleAuthor articleAuthor = new ArticleAuthor();
+                    Author author = new Author();
+                    author.setId(authorContributionDTO.getAuthorId());
+                    articleAuthor.setAuthor(author);
+                    articleAuthor.setContribution(authorContributionDTO.getContribution());
+                    articleAuthor.setArticle(article);
+                    return articleAuthor;
+                })
+                .collect(Collectors.toList()));
+
+        return article;
     }
 }
